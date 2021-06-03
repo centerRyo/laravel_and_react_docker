bb2563d4fa7bfa4e8e344f8e7af3bee159d1378f
centerRyo <abc.atrn@gmail.com>
2021-06-03 11:12:50 +0900

    Hello Reactを表示する

---
+++ laravel_react/resources/js/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <h1>Hello React!</h1>;
+}
+
+if (document.getElementById('app')) {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+  );
+}
